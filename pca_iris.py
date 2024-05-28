2eeacd35cb30ac7ab083d336cf5c3360b3ed48e8
jparep <parepson@gmail.com>
2024-05-28 12:15:52 -0600

    iris data loaded

--- pca_iris.py
+++ pca_iris.py
@@ -14,3 +14,18 @@ scaler = StandardScaler()
 features_scaled = scaler.fit_transform(features)
 
 # Compute Covariance Matric
+cov_mx = np.cov(features_scaled, rowvar=False)
+
+# Computer Eiggen values and Eigen vectors
+eValues, eVectors = np.linalg.eigh(cov_mx)
+
+# Sort eigen values and eigen vectors
+sorted_idx = np.argsort(eValues)[::-1]
+sorted_eValues = eValues[sorted_idx]
+sorted_eVectors = eVectors[:, sorted_idx]
+
+# Select the top 2 eigen vectors (for 2D projection)
+n_components = 2
+selected_eVectors = sorted_eVectors[:, :n_components]
+
+# Transform the data into new space
